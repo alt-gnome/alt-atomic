2c752958289bae109cd39dcde1a4e918e5ba89e8
Vladimir Vaskov <rirusha@altlinux.org>
2025-06-07 20:02:01 +0300

    packages(tasks): Imprive template

--- src/packages/03-tasks-list.sh
+++ src/packages/03-tasks-list.sh
@@ -2,12 +2,22 @@
 
 echo "::group:: ===$(basename "$0")==="
 
+apt-get update
+
 TASKS=(
 
 )
 
-for task in "${TASKS[@]}"; do
-  apt-repo test "$task"
+for task in "${TASKS[@]}"; do 
+    apt-repo add task "$task"
 done
+
+TASKS_PACKAGES=(
+
+)
+
+if [ ${#TASKS_PACKAGES[@]} -gt 0 ]; then
+    apt-get install -y "${TASKS_PACKAGES[@]}"
+fi
 
 echo "::endgroup::"
