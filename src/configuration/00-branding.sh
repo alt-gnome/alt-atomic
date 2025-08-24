e1a3c04169b716acaf1676731c59cb1ba764d304
Vladimir Vaskov <rirusha@altlinux.org>
2025-08-25 02:22:36 +0300

    branding: Make better

--- src/configuration/00-branding.sh
+++ src/configuration/00-branding.sh
@@ -5,19 +5,24 @@ set -e
 echo "::group:: ===$(basename "$0")==="
 
 curver=$(date +%Y%m%d)
+pretty_curver=$(date +%d.%m.%Y)
 
 if [ "$IMAGE_TYPE" = "nightly" ]; then
-    postfix=" Nightly"
+    pretty_type="Nightly"
+    postfix=" $pretty_type"
+    release_type="development"
 else
     postfix=""
+    pretty_type="Stable"
+    release_type="stable"
 fi
 
 # Fix os-release file with current image info
-sed -i "s|NAME=\".*\"|NAME=\"ALT Atomic Onyx$postfix\"|g" /usr/lib/os-release
-sed -i "s|VERSION=\".*\"|VERSION=\"$curver-$IMAGE_TYPE\"|g" /usr/lib/os-release
-sed -i "s|VERSION_ID=\".*\"|VERSION_ID=$curver)|g" /usr/lib/os-release
-sed -i "s|BUILD_ID=\".*\"|BUILD_ID=\"Sisyphus $curver\"|g" /usr/lib/os-release
-sed -i "s|CPE_NAME=\".*\"|CPE_NAME=\"cpe:/o:alt:atomic-onyx:$curver\"|g" /usr/lib/os-release
+sed -i "s|PRETTY_NAME=.*|PRETTY_NAME=\"ALT Atomic Onyx$postfix\"|g" /usr/lib/os-release
+sed -i "s|VERSION=.*|VERSION=\"$pretty_curver $pretty_type\"|g" /usr/lib/os-release
+sed -i "s|VERSION_ID=.*|VERSION_ID=$curver|g" /usr/lib/os-release
+sed -i "s|CPE_NAME=.*|CPE_NAME=\"cpe:/o:alt:atomic:$IMAGE_TYPE:$curver\"|g" /usr/lib/os-release
+sed -i "s|RELEASE_TYPE=.*|RELEASE_TYPE=$release_type|g" /usr/lib/os-release
 
 # Replace file with link
 rm -rf /etc/os-release
