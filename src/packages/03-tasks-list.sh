7e5c80f720b4cd508038e92f6f079f3d93121049
Vladimir Vaskov <rirusha@altlinux.org>
2025-09-22 13:23:55 +0300

    tasks: Remove task

--- src/packages/03-tasks-list.sh
+++ src/packages/03-tasks-list.sh
@@ -3,7 +3,7 @@
 echo "::group:: ===$(basename "$0")==="
 
 TASKS=(
-    394652
+
 )
 
 for task in "${TASKS[@]}"; do 
@@ -13,8 +13,7 @@ done
 apt-get update
 
 TASKS_PACKAGES=(
-    icon-theme-alt-atomic-onyx
-    plymouth-theme-atomic
+
 )
 
 if [ ${#TASKS_PACKAGES[@]} -gt 0 ]; then
